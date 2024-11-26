cda1572dbd8b492f169466b2f7088c34bf1ef306
NoLimits <floreslenguaeduardo@gmail.com>
2024-11-25 20:04:15 -0500

    jacoco, test para findById and findAll services

---
+++ src/test/java/com/example/curso/CategoryDataProvider.java
@@ -0,0 +1,55 @@
+package com.example.curso;
+
+import com.example.curso.application.dto.categoy.CategoryResponse;
+import com.example.curso.domain.enums.StatusEnum;
+import com.example.curso.domain.models.Category;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.List;
+import java.util.Optional;
+
+public class CategoryDataProvider {
+
+    public static Optional<Category> modelMock() {
+        var createDateTime = LocalDateTime.of(2024, Month.DECEMBER, 1,12,12);
+        var updateDateTime = LocalDateTime.of(2024, Month.DECEMBER, 3,15,15);
+
+        return Optional.of(
+                new Category(1L, "Micrófonos", StatusEnum.DISABLED, createDateTime, updateDateTime)
+        );
+    }
+
+    public static CategoryResponse modelResponseMock() {
+        var createDateTime = LocalDateTime.of(2024, Month.DECEMBER, 1,12,12);
+        var updateDateTime = LocalDateTime.of(2024, Month.DECEMBER, 3,15,15);
+
+        return new CategoryResponse(1L, "Micrófonos", StatusEnum.DISABLED, createDateTime, updateDateTime);
+    }
+
+    public static List<Category> modelListMock() {
+        var createDateTime = LocalDateTime.of(2024, Month.DECEMBER, 1,12,12);
+        var updateDateTime = LocalDateTime.of(2024, Month.DECEMBER, 3,15,15);
+
+        return List.of(
+                new Category(1L, "Micrófonos", StatusEnum.DISABLED, createDateTime, updateDateTime),
+                new Category(2L, "Sillas Gamer", StatusEnum.ENABLED, createDateTime, updateDateTime),
+                new Category(3L, "Monitores", StatusEnum.ENABLED, createDateTime, updateDateTime),
+                new Category(4L, "Teclados", StatusEnum.DISABLED, createDateTime, updateDateTime),
+                new Category(5L, "Mouses", StatusEnum.ENABLED, createDateTime, updateDateTime)
+        );
+    }
+
+    public static List<CategoryResponse> modelResponseListMock() {
+        var createDateTime = LocalDateTime.of(2024, Month.DECEMBER, 1,12,12);
+        var updateDateTime = LocalDateTime.of(2024, Month.DECEMBER, 3,15,15);
+
+        return List.of(
+                new CategoryResponse(1L, "Micrófonos", StatusEnum.DISABLED, createDateTime, updateDateTime),
+                new CategoryResponse(2L, "Sillas Gamer", StatusEnum.ENABLED, createDateTime, updateDateTime),
+                new CategoryResponse(3L, "Monitores", StatusEnum.ENABLED, createDateTime, updateDateTime),
+                new CategoryResponse(4L, "Teclados", StatusEnum.DISABLED, createDateTime, updateDateTime),
+                new CategoryResponse(5L, "Mouses", StatusEnum.ENABLED, createDateTime, updateDateTime)
+        );
+    }
+}
---
+++ src/test/java/com/example/curso/application/services/CategoryServiceTest.java
@@ -0,0 +1,108 @@
+package com.example.curso.application.services;
+
+import com.example.curso.CategoryDataProvider;
+import com.example.curso.application.dto.categoy.CategoryResponse;
+import com.example.curso.application.mapper.CategoryServiceMapper;
+import com.example.curso.application.ports.output.CategoryPersistencePort;
+import com.example.curso.domain.exception.NotFoundException;
+import com.example.curso.domain.models.Category;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class CategoryServiceTest {
+
+    @InjectMocks
+    private CategoryService categoryService;
+
+    @Mock
+    private CategoryPersistencePort categoryPersistencePort;
+
+    @Mock
+    private CategoryServiceMapper categoryServiceMapper;
+
+    @Test
+    public void findByIdTest() {
+
+        // Given
+        Long idCategory = 1L;
+        Optional<Category> modelMock = CategoryDataProvider.modelMock();
+        CategoryResponse modelResponseMock = CategoryDataProvider.modelResponseMock();
+
+        // When
+        when(categoryPersistencePort.findById(anyLong())).thenReturn(modelMock);
+        when(categoryServiceMapper.toResponse(any(Category.class))).thenReturn(modelResponseMock);
+        CategoryResponse result = categoryService.findById(idCategory);
+
+        // Then
+        assertNotNull(result);
+        assertEquals(modelMock.get().getId(), result.getId());
+        assertEquals(modelMock.get().getName(), result.getName());
+        assertEquals(modelMock.get().getStatus(), result.getStatus());
+        assertEquals(modelMock.get().getCreatedAt(), result.getCreatedAt());
+        assertEquals(modelMock.get().getUpdatedAt(), result.getUpdatedAt());
+
+        verify(categoryPersistencePort).findById(anyLong());
+        verify(categoryServiceMapper).toResponse(any(Category.class));
+    }
+
+    @Test
+    public void findByIdNotFoundExceptionTest() {
+        // Given
+
+        // When
+        when(categoryPersistencePort.findById(anyLong()))
+                .thenReturn(Optional.empty());
+
+        // Then
+        assertThrows(NotFoundException.class,
+                () -> categoryService.findById(anyLong())
+        );
+
+        verify(categoryPersistencePort).findById(anyLong());
+    }
+
+    @Test
+    public void findAllTest() {
+
+        // Given
+        Pageable pageable = PageRequest.of(0, 5);
+        List<Category> modelListMock = CategoryDataProvider.modelListMock();
+        int sizeList = modelListMock.size();
+        List<CategoryResponse> modelResponseListMock = CategoryDataProvider.modelResponseListMock();
+        Page<Category> categoryPageMock = new PageImpl<>(modelListMock, pageable, modelListMock.size());
+
+        // When
+        when(categoryPersistencePort.findAll(any(Pageable.class))).thenReturn(categoryPageMock);
+        for (int i = 0; i < sizeList; i++) {
+            when(categoryServiceMapper.toResponse(modelListMock.get(i)))
+                    .thenReturn(modelResponseListMock.get(i));
+        }
+        Page<CategoryResponse> result = categoryService.findAll(pageable);
+
+        // Then
+        assertEquals(modelListMock.size(), result.getTotalElements());
+
+        for(int i = 0;  i < sizeList; i++) {
+            assertEquals(modelListMock.get(i).getId(), result.getContent().get(i).getId());
+            assertEquals(modelListMock.get(i).getName(), result.getContent().get(i).getName());
+            assertEquals(modelListMock.get(i).getStatus(), result.getContent().get(i).getStatus());
+        }
+
+        verify(categoryPersistencePort).findAll(any(Pageable.class));
+        verify(categoryServiceMapper, times(modelListMock.size())).toResponse(any(Category.class));
+    }
+}
