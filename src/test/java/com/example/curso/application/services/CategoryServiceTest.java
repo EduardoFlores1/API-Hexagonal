5e5854aa7584ab9dce20dec8735dfa63658665be
NoLimits <floreslenguaeduardo@gmail.com>
2024-11-26 01:32:10 -0500

    cambio a test create avance

--- src/test/java/com/example/curso/application/services/CategoryServiceTest.java
+++ src/test/java/com/example/curso/application/services/CategoryServiceTest.java
@@ -114,7 +114,7 @@ class CategoryServiceTest {
         CategoryRequest modelRequestMock = CategoryDataProvider.newModelMock();
         System.out.println(modelRequestMock.getName());
 
-        // When
+        // When ok
 
         // Then
     }
